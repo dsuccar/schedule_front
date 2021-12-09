696518a442b77e7581c81c09ec9ce9661625408d
dsuccar <danielsuccar@icloud.com>
2021-12-09 15:11:30 -0500

    routes added, recoil installed

---
+++ src/atoms.js
@@ -0,0 +1,23 @@
+import { atom}from 'recoil'
+
+
+export const searchTextState = atom({
+  key: "searchText",
+  default: ""
+})
+
+export const barbersState = atom({
+  key: "barbersState",
+  default: []
+})
+
+
+
+
+// export const clientReviewsState = atom({
+//   key: "clientReviewsState",
+//   default: {}
+// })
+
+
+
