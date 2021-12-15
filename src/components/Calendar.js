71b2460f4bb8df203c88210e4bf614e23a7d8f4d
dsuccar <danielsuccar@icloud.com>
2021-12-15 16:06:11 -0500

    custom weekly calendar dates and css

---
+++ src/components/Calendar.js
@@ -0,0 +1,33 @@
+import moment from 'moment'
+import "../App.css"
+
+export default function Calendar(){
+
+  const value = moment()
+  const startDay = value.clone().startOf("week")
+  const endDay = value.clone().endOf("week")
+  const day = startDay.clone().subtract(1, "day")
+ 
+  const calendar = []
+ 
+  while(day.isBefore(endDay, "day")) {
+    calendar.push(
+      Array(7)
+       .fill(0)
+       .map(() => day.add(1, "day").clone())
+    )
+  }
+
+
+
+  return(
+  <div className="calendar">
+    {calendar.map((week) => 
+        (<div>{week.map ((day)=>( 
+            <div className="day">{day.format("D").toString()}</div>
+            ))}
+        </div>
+      ))}
+  </div>
+  )
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,40 @@
-import React from 'react';
-import { useEffect, useState } from 'react'
-import { useRecoilState } from 'recoil'
-import { usernameState } from '../atoms'
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+// import { useEffect, useState } from 'react'
+// import { useRecoilState } from 'recoil'
+// import { usernameState } from '../atoms'
+import Calendar from './Calendar'
+
+
+
+// import Calendar from 'react-calendar'
+// import 'react-calendar/dist/Calendar.css';
+
+
 // import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
 
 export default function Home(){
+//  const [username, setUsername] = useRecoilState(usernameState)
+//  const [date, setDate] = useState(new Date());
 
- const [username, setUsername] = useRecoilState(usernameState)
- const [date, setDate] = useState(new Date());
+
+
+
+//  useEffect(() => {
+//   setUsername('dsuccar')
+//  },[])
+
  
 
-
-  // const onChange = date => {
-  //   setDate(date)
-  //   console.log("idk why this is still changing")
-  // }
+//  const onChange = date => {
+//   setDate(date)
+// }  
 
   return(
-<div>
- <Calendar
-//  onChange = {onChange}
-//  value = {date}
-/>
-</div>
+    <div>
+      <Calendar/>
+    </div>
+    
+
+
+
   )
 }
